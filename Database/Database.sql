82efddb4a301bb2984f32ea99c759cd6c36e0256
yooh <lewisdee@hotmail.co.uk>
2011-10-18 00:39:25 +0100

    Added 2 tables - Membership + Suppliments

--- Database/Database.sql
+++ Database/Database.sql
@@ -1,6 +1,8 @@
 DROP TABLE Staff;
 DROP TABLE Member;
 DROP TABLE Classes;
+DROP TABLE Membership;
+DROP TABLE Suppliments;
 
 CREATE TABLE Staff (
 StaffId Number(3) PRIMARY KEY,
@@ -19,10 +21,25 @@ lastName VARCHAR(15)NOT NULL,
 dateOfBirth DATE,
 gender CHAR(1) NOT NULL,
 takesClass Number(3),
-FOREIGN KEY (takesClass) REFERENCES Classes(ClassId) ,
+mShipType Number (3) NOT NULL,
+FOREIGN KEY (takesClass) REFERENCES Classes(ClassId),
+FOREIGN KEY (mShipType) REFERENCES Membership(MembershipId),
 )ENGINE=INNODB;
 
 CREATE TABLE Classes (
 ClassId Number(3) PRIMARY KEY,
 ClassName VARCHAR(15) NOT NULL,
+)ENGINE=INNODB;
+
+CREATE Table Membership(
+MembershipId Number(3) PRIMARY KEY,
+MembershipName VARCHAR(15) NOT NULL,
+Price Number(2) NOT NULL,
+)ENGINE=INNODB;
+
+CREATE Table Suppliments(
+SuppId Number (3) PRIMARY KEY,
+SuppName VARCHAR (15) NOT NULL,
+SuppType VARCHAR (10) NOT NULL,
+SuppPrice Number (2) NOT NULL,
 )ENGINE=INNODB;
