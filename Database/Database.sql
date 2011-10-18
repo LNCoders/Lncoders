f828c37d22a657b7d967e00c60491754c1c2eee9
yooh <lewisdee@hotmail.co.uk>
2011-10-18 23:24:05 +0100

    added some comments

--- Database/Database.sql
+++ Database/Database.sql
@@ -1,8 +1,11 @@
+//Drops Each Table
 DROP TABLE Staff;
 DROP TABLE Member;
 DROP TABLE Classes;
 DROP TABLE Membership;
 DROP TABLE Suppliments;
+
+// Creates the Staff table with (ID,Firstname,Surname,Gender,Teaches)
 
 CREATE TABLE Staff (
 StaffId INT (3) PRIMARY KEY,
@@ -12,6 +15,7 @@ gender CHAR(1) NOT NULL,
 teaches INT (3) 
 );
 
+// Creates the Member table with (ID,Firstname,Surname,D.O.B,Gender,takesClass,mShipType)
 
 CREATE TABLE Member (
 MemberId INT (3) PRIMARY KEY,
@@ -22,18 +26,18 @@ gender CHAR(1) NOT NULL,
 takesClass INT(3),
 mShipType INT(3) NOT NULL
 );
-
+// Creates the Classes table with (ID,ClassName)
 CREATE TABLE Classes (
 ClassId INT(3) PRIMARY KEY,
 ClassName VARCHAR(15) NOT NULL
 );
-
+// Creates the Classes table with (ID,MembershipName,Price)
 CREATE TABLE Membership(
 MembershipId INT (3) PRIMARY KEY,
 MembershipName VARCHAR(15) NOT NULL,
 Price DECIMAL(3,2) NOT NULL
 );
-
+// Creates the Suppliments table with (ID,suppName,suppType,suppPrice)
 CREATE TABLE Suppliments(
 SuppId INT (3) PRIMARY KEY,
 SuppName VARCHAR (15) NOT NULL,
