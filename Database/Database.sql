30ec9b43e2038461fb99b916f10ca659ba09eaca
yooh <lewisdee@hotmail.co.uk>
2011-10-18 23:12:25 +0100

    Changed a Few things, the tables now create in MySql :)

--- Database/Database.sql
+++ Database/Database.sql
@@ -9,9 +9,8 @@ StaffId INT (3) PRIMARY KEY,
 firstName VARCHAR(15) NOT NULL,
 lastName VARCHAR(15)NOT NULL,
 gender CHAR(1) NOT NULL,
-teaches Number (3), 
-FOREIGN KEY (teaches) REFERENCES Classes(ClassId) ,
-)ENGINE=INNODB;
+teaches INT (3) 
+);
 
 
 CREATE TABLE Member (
@@ -20,26 +19,24 @@ firstName VARCHAR(15) NOT NULL,
 lastName VARCHAR(15)NOT NULL,
 dateOfBirth DATE NOT NULL,
 gender CHAR(1) NOT NULL,
-takesClass DECIMAL(3),
-mShipType DECIMAL(3) NOT NULL,
-FOREIGN KEY (takesClass) REFERENCES Classes(ClassId),
-FOREIGN KEY (mShipType) REFERENCES Membership(MembershipId),
-)ENGINE=INNODB;
+takesClass INT(3),
+mShipType INT(3) NOT NULL
+);
 
 CREATE TABLE Classes (
 ClassId INT(3) PRIMARY KEY,
-ClassName VARCHAR(15) NOT NULL,
-)ENGINE=INNODB;
+ClassName VARCHAR(15) NOT NULL
+);
 
-CREATE Table Membership(
+CREATE TABLE Membership(
 MembershipId INT (3) PRIMARY KEY,
 MembershipName VARCHAR(15) NOT NULL,
-Price DECIMAL(3,2) NOT NULL,
-)ENGINE=INNODB;
+Price DECIMAL(3,2) NOT NULL
+);
 
-CREATE Table Suppliments(
+CREATE TABLE Suppliments(
 SuppId INT (3) PRIMARY KEY,
 SuppName VARCHAR (15) NOT NULL,
 SuppType VARCHAR (10) NOT NULL,
-SuppPrice DECIMAL (3,2) NOT NULL,
-)ENGINE=INNODB;
+SuppPrice DECIMAL (3,2) NOT NULL
+);
