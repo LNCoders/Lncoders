b406b1445abb8aaf5aab24a717cbf4d93b6922ff
Scott Jameson <DOWmad@gmail.com>
2012-01-11 10:23:52 +0000

    App and database

---
+++ App, database, queries, test data/queries.sql
@@ -0,0 +1,1 @@
+SELECT * FROM supplements, membership, member, classes, fitnessplans;
---
+++ App, database, queries, test data/test data.sql
@@ -0,0 +1,6 @@
+INSERT INTO fitness.classes VALUES (019,'gym', 'Exercise', 12, 2, '1991-02-02', 300);
+INSERT INTO fitness.fitnessplans VALUES (012, 'healthy', 'aggressive');
+INSERT INTO fitness.membership VALUES (013, 'nudda', 2.5, 'Junior');
+INSERT INTO fitness.supplements VALUES (014, 'Protein', 'Muscular', 3.2, 4);
+INSERT INTO fitness.member VALUES (016, 'Sam', 'Ganjy', 'M', '1999-02-02', 013, 019, 014,012);
+INSERT INTO fitness.staff VALUES (010,'Lit', 'Lat', 'F', 019, 'FInstructor');
